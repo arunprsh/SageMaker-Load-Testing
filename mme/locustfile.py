2c0adaddef2626f5c5705ab015b8bc68ad5be510
Arunprasath Shankar <arunprsh@amazon.com>
2021-03-31 16:00:05 -0500

    adding example for MME

---
+++ mme/locustfile.py
@@ -0,0 +1,47 @@
+from locust import HttpUser, task
+from authorizer import authorize
+import config as conf
+import json
+import time
+
+# For content type=text/csv use below line
+# PAYLOAD = '0.234234,0.234345,0.5634,-0.23535'
+
+# For content type=application/json use similar to below 2 lines
+PAYLOAD = {'instances': [[2.804030096111861,
+                          -0.5078493396918523,
+                          0.9696087678038148,
+                          -0.3296902366978934,
+                          1.2317458148495393,
+                          0.11934137168357777,
+                          1.1473978828301732,
+                          -0.9193527632708587,
+                          1.6060928595608375,
+                          1.4077822707538348,
+                          0.9051304120610045,
+                          -4.278295167692499,
+                          2.513247725276389]]}
+PAYLOAD = json.dumps(PAYLOAD)
+
+
+class WebsiteUser(HttpUser):
+    min_wait = 1
+    max_wait = 5  # time in ms
+
+    @task(20)  # Model-1 gets called 20x times more than Model-2
+    def test_post_model_1(self):
+        """
+        Load Test SageMaker Endpoint (POST request)
+        """
+        headers = authorize(PAYLOAD, 'model1.tar.gz')
+        self.client.post(conf.SAGEMAKER_ENDPOINT_URL, data=PAYLOAD, headers=headers, name='Post Request')
+        # time.sleep(5)
+
+    @task(1)
+    def test_post_model_2(self):
+        """
+        Load Test SageMaker Endpoint (POST request)
+        """
+        headers = authorize(PAYLOAD, 'model2.tar.gz')
+        self.client.post(conf.SAGEMAKER_ENDPOINT_URL, data=PAYLOAD, headers=headers, name='Post Request')
+        # time.sleep(15)
